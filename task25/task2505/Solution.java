a7c06f2b76cd82c8a140b8a4374a25293dcdba68
Askue <work@work.com>
2018-11-15 14:55:38 +0300

    new comp

--- task25/task2505/Solution.java
+++ task25/task2505/Solution.java
@@ -5,17 +5,8 @@ package com.javarush.task.task25.task2505;
 */
 public class Solution {
 
-    /*private class MyUncaughtExceptionHandler extends Thread {
 
-        Thread thread = new Thread();
-
-        *//*public MyUncaughtExceptionHandler () {
-
-        }*//*
-
-    }*/
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException  {
         MyThread myThread = new Solution().new MyThread("super secret key");
         myThread.start();
     }
@@ -23,14 +14,32 @@ public class Solution {
     public class MyThread extends Thread {
         private String secretKey;
 
-        private class MyUncaughtExceptionHandler {
+        private class MyUncaughtExceptionHandler implements UncaughtExceptionHandler {
 
+            String name;
+            Thread t;
+
+           public MyUncaughtExceptionHandler() {
+               try {
+                   t = new Thread();
+                   Thread.sleep(500);
+                   System.out.print(secretKey + ", " + Thread.currentThread().getName() + ", ");
+               } catch (InterruptedException e) {
+                   e.printStackTrace();
+               } finally {
+                   System.out.println();
+               }
+           }
+
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+            }
         }
 
         public MyThread(String secretKey) {
             this.secretKey = secretKey;
             setUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
-            setDaemon(true);
+            setDaemon(false);
         }
 
         @Override
